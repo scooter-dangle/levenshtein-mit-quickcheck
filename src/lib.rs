2b2502b4f2ad80a458112eba29ff2ae530f69b0b
Scott Steele <scott.steele@distilnetworks.com>
2016-09-29 17:29:54 -0400

    This is all original work. Please pay me for this great algorithm.
    Multiple patents pending.

--- src/lib.rs
+++ src/lib.rs
@@ -4,67 +4,19 @@ pub const SUBSTITUTION_COST: usize = 1;
 pub const IDENTITY_COST:     usize = 0;
 
 pub fn distance(lhs: &str, rhs: &str) -> usize {
-    //
-    // TODO use C instead? :( Why no Wikipedia have Rust for copypaste?!
-    //
-    // int LevenshteinDistance(string s, string t)
-    // {
-    //     // degenerate cases
-    //     if (s == t) return 0;
-    //
+    // Thanks, Mr Wales!
+
     if lhs == rhs { return 0; }
 
-    //
-    //     if (s.Length == 0) return t.Length;
-    //     if (t.Length == 0) return s.Length;
-    //
     if lhs.is_empty() || rhs.is_empty() { return 0; }
 
-    //
-    //     // create two work vectors of integer distances
-    //     int[] v0 = new int[t.Length + 1];
-    //     int[] v1 = new int[t.Length + 1];
-    //
     let mut v0 = (0..(lhs.len() + 1)).collect::<Vec<_>>();
     let mut v1 = v0.clone();
 
-    //
-    //     // initialize v0 (the previous row of distances)
-    //     // this row is A[0][i]: edit distance for an empty s
-    //     // the distance is just the number of characters to delete from t
-    //     for (int i = 0; i < v0.Length; i++)
-    //         v0[i] = i;
-    //
-
-    //                ∧
-    //                |
-    //  Already done, that, yo.
-
-    //
-    //     for (int i = 0; i < s.Length; i++)
-    //     {
-    //
     for (r_index, r_char) in rhs.chars().enumerate() {
-
-    //
-    //         // calculate v1 (current row distances) from the previous row v0
-    //
-    //         // first element of v1 is A[i+1][0]
-    //         //   edit distance is delete (i+1) chars from s to match empty t
-    //         v1[0] = i + 1;
-    //
         v1[0] = r_index + INSERTION_COST;
 
-    //
-    //         // use formula to fill in the rest of the row
-    //         for (int j = 0; j < t.Length; j++)
-    //         {
-    //             var cost = (s[i] == t[j]) ? 0 : 1;
-    //             v1[j + 1] = Minimum(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost);
-    //         }
-    //
         for (l_index, l_char) in lhs.chars().enumerate() {
-            // Did teh things here
             let cost = if l_char == r_char {
                 IDENTITY_COST
             } else {
@@ -82,19 +34,8 @@ pub fn distance(lhs: &str, rhs: &str) -> usize {
             );
         }
 
-    //
-    //         // copy v1 (current row) to v0 (previous row) for next iteration
-    //         for (int j = 0; j < v0.Length; j++)
-    //             v0[j] = v1[j];
-    //     }
-    //
         v0.clone_from_slice(&v1);
-
     }
-    //
-    //     return v1[t.Length];
-    // }
-    //
 
     *v1.last().unwrap()
 }
